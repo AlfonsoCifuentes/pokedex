ba998a1f07ef51cfe64391e46eb514a254bdb40a
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-11-06 15:09:17 +0100

    Segunda Version

--- pokedex.js
+++ pokedex.js
@@ -7,37 +7,44 @@
         let elementoLi = document.createElement("li");
         elementoLi.setAttribute("class", "card");
 
+        /* título de cada tarjeta */
+
         let divTitulo = document.createElement("div");
         elementoLi.appendChild(divTitulo);
         divTitulo.setAttribute("class", "card-title");
+
+        let textoNombre = document.createTextNode(drawPokemon.name);
+        divTitulo.appendChild(textoNombre);
+
+        /* subtítulo de cada tarjeta, en este caso la experiencia base */
 
         let divSubtitulo = document.createElement("div");
         elementoLi.appendChild(divSubtitulo);
         divSubtitulo.setAttribute("class", "card-subtitle");
 
-        let textoSubtitulo = document.createTextNode("Experiencia: "+ drawPokemon.base_experience);
+        let textoSubtitulo = document.createTextNode("XP: "+ drawPokemon.base_experience);
         divSubtitulo.appendChild(textoSubtitulo);
 
-       
-        let textoNombre = document.createTextNode(drawPokemon.name);
-        divTitulo.appendChild(textoNombre);
-        
+        /* imagen del pokemon  */
         
         let imagenPokemon = document.createElement("img");
         imagenPokemon.setAttribute("src", drawPokemon.sprites.front_default );
         imagenPokemon.setAttribute("class", "card-image");
         elementoLi.appendChild(imagenPokemon);
 
+        /* añadiendo el elemento li con todo lo anterior al div con id pokedex */
+
         document.getElementById("pokedex").appendChild(elementoLi);
         
-
-       
         
       })
-      .then ((testPokemon) => {console.log(testPokemon);}) //for testing purposes only, DELETE AFTER COMPLETE
+
+    
      
     }
     
+    /* bucle para iterar sobre la función getPokemon */
+
     for (i=1; i<=150; i++){
        getPokemon ("https://pokeapi.co/api/v2/pokemon/"+i)
     }
