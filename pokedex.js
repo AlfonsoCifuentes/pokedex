c15e8d622fcf5f930cef3ee5ecd9c1f504c2deae
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-11-13 12:43:10 +0100

    Cuarta Versión

--- pokedex.js
+++ pokedex.js
@@ -1,9 +1,33 @@
 ////////////////POKEDEX////////////////////
+
+//Definiendo un array vacío
+const allPokeUrls = [];
+
+//Metiendo cada una de las URL de cada pokemon en el array allPoleUrls
+for (i=1; i<=150; i++){
+  allPokeUrls.push ("https://pokeapi.co/api/v2/pokemon/"+i)
+}
+
+//Mapeando el fetch para cada una de las urls del array
+const allPokeFetch = allPokeUrls.map (pokemonURL => fetch(pokemonURL)
+
+//Convirtiendo la respuesta a JSON
+.then ((response) => response.json())
+
+)
+
+//Metiendo la acción del fetch de las urls en un promise
+Promise.all(allPokeFetch)
+
+//Cuando finaliza la promesa nos devuelve un array de promesas, y empezamos la función que dibujará los pokemon
+.then ((drawPokemon) => {
+
+  //Bucle para iterar sobre el array de promesas
+  for (i=0; i<drawPokemon.length; i++){
     
-    function getPokemon(url){
-      fetch (url)
-      .then((response) => {return response.json();})
-      .then ((drawPokemon) => {
+      
+      //Operando el DOM
+
         let elementoLi = document.createElement("li");
         elementoLi.setAttribute("class", "card");
 
@@ -13,7 +37,7 @@
         elementoLi.appendChild(divTitulo);
         divTitulo.setAttribute("class", "card-title");
 
-        let textoNombre = document.createTextNode(drawPokemon.name);
+        let textoNombre = document.createTextNode(drawPokemon[i].name);
         divTitulo.appendChild(textoNombre);
 
         /* subtítulo de cada tarjeta, en este caso la experiencia base */
@@ -22,32 +46,23 @@
         elementoLi.appendChild(divSubtitulo);
         divSubtitulo.setAttribute("class", "card-subtitle");
 
-        let textoSubtitulo = document.createTextNode("XP: "+ drawPokemon.base_experience);
+        let textoSubtitulo = document.createTextNode("XP: "+ drawPokemon[i].base_experience);
         divSubtitulo.appendChild(textoSubtitulo);
 
         /* imagen del pokemon  */
         
         let imagenPokemon = document.createElement("img");
-        imagenPokemon.setAttribute("src", drawPokemon.sprites.front_default );
+        imagenPokemon.setAttribute("src", drawPokemon[i].sprites.front_default );
         imagenPokemon.setAttribute("class", "card-image");
         elementoLi.appendChild(imagenPokemon);
 
         /* añadiendo el elemento li con todo lo anterior al div con id pokedex */
 
         document.getElementById("pokedex").appendChild(elementoLi);
-        
-        
-      })
+   }
+  }
+);
 
-    
-     
-    }
-    
-    /* bucle para iterar sobre la función getPokemon */
-
-    for (i=1; i<=150; i++){
-       getPokemon ("https://pokeapi.co/api/v2/pokemon/"+i)
-    }
 
 
 
